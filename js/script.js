2bcc137defebd926fb9634df240a8ae5748f5159
koccyx <rings@mail.ru>
2023-07-19 00:04:57 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 import { API_KEY } from "./env.js";
-
-let BASE_URL = 'http://api.exchangeratesapi.io/v1/latest' + `?access_key=${API_KEY}`;
+let BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}`;
 
 async function getRates(url) {
     try {
-        let response = await fetch(url);
+        let response = await fetch(`${BASE_URL}/latest/EUR`);
         let data = await response.json()
 
         return data;
@@ -14,18 +13,49 @@ async function getRates(url) {
     }
 }
 
-const $selectTop = document.querySelector('#top-select');
-const $selectBottom = document.querySelector('#top-select');
+async function converting(url) {
+    try {
+        let topVal = $inputTop.value;
+        let selectTopVal= $selectTop.value;
+        let selectBottomVal= $selectBottom.value; 
+        if(!topVal) throw new Error('Input is empty');
+        let response = await fetch(`${BASE_URL}/pair/${selectTopVal}/${selectBottomVal}/${topVal}`);
+        let result = await response.json();
 
-getRates(BASE_URL)
+        $inputBottom.value = result.conversion_result;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+
+const $selectTop = document.querySelector('#top-select');
+const $selectBottom = document.querySelector('#bottom-select');
+const $inputTop = document.querySelector('.top-input');
+const $inputBottom = document.querySelector('.bottom-input');
+const $button = document.querySelector('.container-button');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    getRates(BASE_URL)
     .then(data => {
-        const rates = Object.keys(data.rates); 
-        
+        const rates = Object.keys(data.conversion_rates); 
+        console.log(data);    
         rates.forEach(rate => {
             const $option = document.createElement('option');
             $option.value = rate;
-            $option.innerText = rate;  
+            $option.innerText = rate; 
+            const $optionClone = $option.cloneNode(true);
             $selectTop.append($option);
-            $selectBottom.append($option);
+            $selectBottom.append($optionClone);
         })
     });
+})
+
+
+$button.addEventListener('click', () => {
+    converting(BASE_URL);
+})
+
+
